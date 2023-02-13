a50a63e697bf5457385d4d7757874082c15a8ef7
Liam van der Viven <liam@vanderviven.com>
2023-02-14 00:22:28 +0100

    fix single to double quotes version.py

--- mkpyp/templates.py
+++ mkpyp/templates.py
@@ -301,11 +301,11 @@ def license(out: io.TextIOBase, props: LicenseProps) -> None:
 
 template_version_py = Template(
     """
-__all__ = 'VERSION', 'version_info'
+__all__ = "VERSION", "version_info"
 
-VERSION = '$VERSION'
+VERSION = "$VERSION"
 
-# OPTDEP: Add optional dependencies for your users here, e. g. 'devtools', 'typing-extensions'.
+# OPTDEP: Add optional dependencies for your users here, e. g. "devtools", "typing-extensions".
 opt_in_dependencies: list[str] = []
 
 def version_info() -> str:
@@ -317,19 +317,19 @@ def version_info() -> str:
     optional_deps = []
     for p in opt_in_dependencies:
         try:
-            import_module(p.replace('-', '_'))
+            import_module(p.replace("-", "_"))
         except ImportError:
             continue
         optional_deps.append(p)
 
     info = {
-        'pydantic version': VERSION,
-        'install path': Path(__file__).resolve().parent,
-        'python version': sys.version,
-        'platform': platform.platform(),
-        'optional deps. installed': optional_deps,
+        "pydantic version": VERSION,
+        "install path": Path(__file__).resolve().parent,
+        "python version": sys.version,
+        "platform": platform.platform(),
+        "optional deps. installed": optional_deps,
     }
-    return '\\n'.join('{:>30} {}'.format(k + ':', str(v).replace('\\n', ' ')) for k, v in info.items())
+    return "\\n".join("{:>30} {}".format(k + ":", str(v).replace("\\n", " ")) for k, v in info.items())
 """.strip()
 )
 
